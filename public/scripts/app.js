e2a0838e7ee2cf533d6c2d0909f38c7c036b86a8
vagrant <stan.solomean@gmail.com>
2019-01-23 22:54:16 +0000

    header not transparent anymore, implemented the function of posting new tweets and getting them back

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -17,70 +17,6 @@ window.onbeforeunload = function() {
 };
 
 
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
-function createTextElement(text) {
-  return `<article class="tweet-article">
-  <img class="logo" src="../images/smiley01.svg">
-  <header>
-    USERNAME
-      <span class="at">@user</span>
-  </header>
-  <p> 
-  ${text}
-  </p>
-  <footer>
-    Created at: NOW
-  </footer>
-</article>`
-}
-
 function createTweetElement(tweet) {
   return `<article class="tweet-article">
   <img class="logo" src="${tweet.user.avatars.small}">
@@ -113,36 +49,35 @@ $(function() {
   $tweetForm.on("submit", function(event) {
     event.preventDefault();
     const $tweetText = $('#new-text');
-    console.log($tweetText)
 
     $.ajax({
       url: '/tweets',
       type: 'POST',
       data: $tweetText.serialize(),
       success: function () {
-
-        let actualText = this.data.substring(5, this.data.length)
-        $('#article').append(createTextElement(actualText))
+        $('#article').empty();
+        loadTweets();
       },
       error: function () {
           alert("error at POST");
       }
-    });
-    // $.ajax({
-    //   url: '/',
-    //   type: 'GET',
-    //   data: $tweetText.serialize(),
-    //   success: function () {
-    //     renderTweets()
-    //   },
-    //   error: function () {
-    //     alert("error at GET")
-    //   }  
-    // }); 
-
+    }); 
   });
-});
 
-$(function() {
-  renderTweets(data);
+  function loadTweets() {
+    $.ajax({
+      url: "/tweets",
+      type: 'GET',
+      dataType: 'json',
+      success: function (data) {
+        renderTweets(data)
+      },
+        error: function() {
+        alert(error);
+      }
+    });
+  }
+
+  loadTweets();
+
 });
