3adcf079c74d96f7d2702e9fc6fd5b4b5e38f3cc
vagrant <stan.solomean@gmail.com>
2019-01-23 18:39:44 +0000

    tweets window implemented, with hover effect

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
 
     $('#new-text').on('input', function(event){ 
         let $current = $(this).val().length;
-        let $counter = $('#new-text').siblings(".counter");
+        let $counter = $(this).siblings(".counter");
         $counter.text(140 - $current);
         
-        if ($counter.text() > 0) {
+        if ($counter.text() >= 0) {
             $counter.css('color', 'gray');
         }
         if ($counter.text() < 0) {
