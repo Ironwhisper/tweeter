bbfe789dd082341fac1aa3b2399474e3751b278d
vagrant <stan.solomean@gmail.com>
2019-01-23 20:33:25 +0000

    modifying tweet button with AJAX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -65,46 +65,44 @@ const data = [
   }
 ];
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-    },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
-
-
 function createTweetElement(tweet) {
-  return tweet.content.text;
+  return `<article class="tweet-article">
+  <img class="logo" src="${tweet.user.avatars.small}">
+  <header>
+    ${tweet.user.name}
+      <span class="at">${tweet.user.handle}</span>
+  </header>
+  <p> 
+  ${tweet.content.text}
+  </p>
+  <footer>
+    Created at: ${tweet.created_at}
+  </footer>
+</article>`
 }
 
-var $tweet = createTweetElement(tweetData);
+// loops through tweets, calls createTweetElement for each tweet, then 
+//takes return value and appends it to the tweets container
+function renderTweets(tweets) {
+  tweets.forEach(tweetX => $('#article').append(createTweetElement(tweetX)))
+}
 
-$('#tweet-article p').append($tweet); 
+$(function() {
+  renderTweets(data);
+});
 
-// function renderTweets(tweets) {
+$(function() {
+  
+  const $tweetButton = $('#tweet-button');
 
-//   tweets.forEach(tweetX => $('#tweet-article p').append(createTweetElement(tweetX)))
-//   // loops through tweets
-//     // calls createTweetElement for each tweet
-//     // takes return value and appends it to the tweets container
-// }
+  $tweetButton.on("submit", function(event) {
+    event.preventDefault;
+    console.log($(this).serialize())
+    // $.ajax('', {method: 'POST'})
+    $.ajax('/tweets/', { method: 'POST' })
+    .then(function (morePostsHtml) {
+      console.log('Success: ', morePostsHtml);
+      $button.replaceWith(morePostsHtml);
 
-// return renderTweets(data);
-
-// <header>
-// header text
-// <span class="at">@somebody</span>
-// </header>
-// <p>
-// </p>
-// <footer>
-// 10 million years ago
+  });
+});
