c52797b6909a4e5b38b3a7e8ab74eb45416bb3ca
vagrant <stan.solomean@gmail.com>
2019-01-24 22:29:14 +0000

    comments

--- server/index.js
+++ server/index.js
@@ -1,26 +1,24 @@
 "use strict";
 
-// Basic express setup:
-
+// Basic express setup
 const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
-
+// Mongodb setup
 const MongoClient = require("mongodb").MongoClient;
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Connecting Mongodb and the database to server
 MongoClient.connect(MONGODB_URI, (err, db) => {
   if (err) {
     console.error(`Failed to connect: ${MONGODB_URI}`);
     throw err;
   }
-  console.log(`Connected to mongodb: ${MONGODB_URI}`);
-
-  //
+  // Datahelpers module with save and get tweets funcitons, operating on the db that is passed into it
   const DataHelpers = require("./lib/data-helpers.js")(db);
 
   // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
@@ -29,23 +27,9 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
 
   // Mount the tweets routes at the "/tweets" path prefix:
   app.use("/tweets", tweetsRoutes);
-
-  // db.close();
-
-
 });
 
-// The in-memory database of tweets. It's a basic object with an array in it.
-// const db = 
-
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
-// 
+// Server initiate function
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
